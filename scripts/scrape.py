f6bcc6a485207600cad6b4a8f2045bacca305ae9
Al Johri <al.johri@gmail.com>
2018-10-04 04:00:17 -0400

    centralize data into governors.yaml file

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
 import us
-import tqdm
+import logging
 import requests
 import datetime
 import lxml.html
+import itertools
 import urllib.parse as urlparse
 
+from nameparser import HumanName
 from requests.adapters import HTTPAdapter
 from requests_futures.sessions import FuturesSession
 from concurrent.futures import as_completed
 
-session = FuturesSession()
+session = FuturesSession(max_workers=10)
 session.mount('https://classic.nga.org', HTTPAdapter(max_retries=15))
 
 KNOWN_EXCEPTIONS = {
@@ -80,42 +82,134 @@ def get_pagination_cursor():
     return cursor
 
 def scrape_page(cursor, offset, pagesize):
+
+    page = offset//pagesize+1
+
     params = {
         'begin'+cursor: offset,
         'pagesize'+cursor: pagesize,
         'end'+cursor: offset+pagesize-1,
         'submit': 'Search'
     }
+
     response = requests.get("https://classic.nga.org/cms/home/governors/past-governors-bios.default.html", params=params)
     doc = lxml.html.fromstring(response.content)
     rows = {x['url']:x for x in parse_search_page(doc)}
     futures = {session.get(url, timeout=10):url for url in rows}
-    for future in tqdm.tqdm(as_completed(futures), total=len(rows)):
+    for i, future in enumerate(as_completed(futures)):
         url = futures[future]
         row = rows[url]
+        logging.info(f'[Page {page}: {i+1} of {len(rows)}] downloaded {url}')
         response = future.result()
         doc = lxml.html.fromstring(response.content)
         detailed_row = parse_bio_page(doc, row)
         yield detailed_row
 
-def scrape(offset=0, pagesize=1000):
-    print("Making initial query to find pagination cursor...")
-    cursor = get_pagination_cursor()
-    print(f"Cursor Found: {cursor}")
+def scrape(offset=0, pagesize=1000, limit=None):
 
-    while True:
-        print(f"Downloading Page {offset//pagesize+1} (offset: {offset}, pagesize: {pagesize})...")
-        num_rows = 0
-        for row in scrape_page(cursor, offset, pagesize):
-            num_rows += 1
-            yield row
-        if num_rows < pagesize: break
-        offset += pagesize
+    def inner(offset, pagesize, limit):
+        print("Making initial query to find pagination cursor...")
+        cursor = get_pagination_cursor()
+        print(f"Cursor Found: {cursor}")
+
+        while True:
+            print(f"Downloading Page {offset//pagesize+1} (offset: {offset}, pagesize: {pagesize})...")
+            num_rows = 0
+            for row in scrape_page(cursor, offset, pagesize):
+                num_rows += 1
+                yield row
+            if num_rows < pagesize: break
+            offset += pagesize
+
+    yield from itertools.islice(inner(offset, pagesize, limit), limit)
+
+def date2year(date):
+    return int(date.split('-')[0]) if date else None
+
+def transform(row):
+    """
+    transforms the raw row into a row that looks like the congress legislators data
+    """
+    parsed_name = HumanName(row['name'])
+    return {
+        'id': {
+            'nga': row['url']
+        },
+        'name': {
+            'original': row['name'],
+            'full': str(parsed_name),
+            **parsed_name.as_dict(include_empty=False),
+        },
+        'bio': {
+            'birthday': row['birthday']
+        },
+        'terms': [
+            {
+                'type': 'gov',
+                'start': term['start'],
+                'end': term['end'],
+                'party': row['party'],
+                'state': row['state'],
+            }
+            for term in row['terms']
+        ]
+    }
+
+def touch(filename):
+    open(filename, 'a').close()
+
+# https://stackoverflow.com/questions/7204805/dictionaries-of-dictionaries-merge/7205107#7205107
+def merge(a, b, path=None):
+    "merges b into a"
+    if path is None: path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge(a[key], b[key], path + [str(key)])
+            elif a[key] == b[key]:
+                pass # same leaf value
+            else:
+                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+        else:
+            a[key] = b[key]
+    return a
 
 if __name__ == "__main__":
-    import json
 
-    start = 0
-    with open('raw.json', 'w') as f:
-        for row in scrape(offset=start):
-            f.write(json.dumps(row) + "\n")
+    logging.basicConfig(level='INFO')
+
+    from votesmart_id_finder import find as find_votesmart_id
+    from ap_id_finder import find as find_ap_id
+
+    import oyaml as yaml
+
+    touch('governors.yaml')
+    with open('governors.yaml') as f:
+        data = yaml.load(f)
+        governors = {x['id']['nga']:x for x in data} if data else {}
+
+    for row in scrape(pagesize=1000):
+        row = transform(row)
+        id_ = row['id']['nga']
+        if id_ in governors:
+            governors[id_] = merge(governors[id_], row)
+        else:
+            governors[id_] = row
+        gov = governors[id_]
+
+        currently_in_office = not bool(gov['terms'][-1]['end'])
+        if currently_in_office or date2year(gov['terms'][-1]['start']) >= 1995:
+            if 'votesmart' not in gov['id']:
+                logging.debug(f'finding votesmart for {id_}')
+                gov['id']['votesmart'] = find_votesmart_id(
+                    name=gov['name']['original'],
+                    currently_in_office=currently_in_office,
+                    term_start=date2year(gov['terms'][-1]['start']),
+                    term_end=date2year(gov['terms'][-1]['end']),
+                    valid_office_types=['Governor'])
+            if 'ap' not in gov['id']:
+                gov['id']['ap'] = find_ap_id(name=gov['name']['original'])
+
+    output_rows = sorted(governors.values(), key=lambda x: (x['terms'][-1]['end'] or '9999', x['terms'][-1]['state']), reverse=True)
+    with open('governors.yaml', 'w') as f:
+        yaml.dump(output_rows, f, default_flow_style=False)
--- scripts/validate.py
+++ scripts/validate.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 
 import us
-import json
-
-from ap_id_finder import find as find_ap_id
+import yaml
 
 STATES = {x.abbr for x in us.STATES} - {'DC'}
 
-with open('raw.json') as f:
-    governors = [json.loads(line) for line in f]
+with open('governors.yaml') as f:
+    governors = yaml.load(f)
 
 current_governors = [
-    x for x in governors if not x['terms'][-1]['end'] if x['state'] in STATES
+    x for x in governors if not x['terms'][-1]['end'] if x['terms'][-1]['state'] in STATES
 ]
 
 assert len(current_governors) == 50
 
 print("Governors by States:")
-for i, governor in enumerate(sorted(current_governors, key=lambda x: x['state'])):
-    npid = find_ap_id(name=governor['name'])
-    print(i, governor['state'], governor['name'], '\t', npid)
+for i, governor in enumerate(sorted(current_governors, key=lambda x: x['terms'][-1]['state'])):
+    print(i, governor['terms'][-1]['state'], governor['name']['full'], '\t', governor['id']['ap'])
